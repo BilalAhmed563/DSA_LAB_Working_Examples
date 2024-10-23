c1b59bf5ca072f87f4cb9fe386d8de7898ac0120
BilalAhmed563 <ba9542689@gmail.com>
2024-10-22 21:33:42 -0700

    Add files via upload

---
+++ DSA_LAB.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dsa_lab;
+
+/**
+ *
+ * @author 123
+ */
+class DSA_LAB {
+    public static void main(String[] args) {
+        int sum = 0; 
+        for (int i = 1; i <= 10; i++) {
+            sum += i; 
+        }
+        System.out.println(sum);
+    }
+}
+
